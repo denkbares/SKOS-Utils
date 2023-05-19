43cf55d280b1c2ed113aa769d51726a5bc866445
Valentin <valentin.ross@stud-mail.uni-wuerzburg.de>
2023-05-19 09:20:34 +0200

    Add python tests and testdata, update .gitignore

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+# Get the path to the requirements.txt file
+requirements_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt")
+
+# Read the requirements.txt file
+with open(requirements_path, "r") as f:
+    requirements = f.read().splitlines()
+
+# Setup configuration
+setup(
+    name="SKOSTools",
+    version="0.1",
+    install_requires=requirements,
+)
