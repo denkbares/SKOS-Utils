113cfc8a64a7827c8570991d63878a75ddd5eb08
Joachim Baumeister <joachim.baumeister@denkbares.com>
2023-07-27 15:25:25 +0200

    Udpated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 # Get the path to the requirements.txt file
 requirements_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "requirements.txt")
@@ -13,4 +13,12 @@ setup(
     name="SKOS-Utils",
     version="0.1",
     install_requires=requirements,
+    packages=find_packages(),
+    description='Tool suite for developing and checking SKOS vocabulaires.',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License',
+        'Programming Language :: Python :: 3.11',
+    ]
 )
